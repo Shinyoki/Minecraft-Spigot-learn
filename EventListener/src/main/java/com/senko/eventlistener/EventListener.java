82756b8525d91895fbc9a94e6e78befd7ca0ad8b
senko <1641801056@qq.com>
2022-03-18 08:53:19 +0800

    项目初始化

---
+++ EventListener/src/main/java/com/senko/eventlistener/EventListener.java
@@ -0,0 +1,17 @@
+package com.senko.eventlistener;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EventListener extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
